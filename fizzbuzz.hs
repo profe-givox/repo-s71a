6e6e286dade41189e112852eb6c2fec5ef7a3167
perro-negro <gi.vega@itsur.edu.mx>
2017-09-13 15:58:23 -0500

    se adiciono la funcion numbers

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -9,4 +9,13 @@ tens :: Int -> String
 tens n 
 	| n>=2 && n<=9 = 
 	answers!!(n-2)
-	where answers=["twenty","thirty","forty","fifty"]
+	where answers= words("twenty thirty forty fifty sixty seventy eighty ninety")
+
+numbers :: Int -> String
+numbers n
+	| n < 20 = lessThan20 n
+	| n < 100 && ((n `mod` 10)==0) = tens (n `div` 10)
+	| n < 100 && ((n `mod` 10) /= 0) = tens (n `div` 10) ++ " " ++ lessThan20 (n `mod` 10)
+	| n == 100 = "One hundred"
+
+fizzbuzz :: Int ->String
